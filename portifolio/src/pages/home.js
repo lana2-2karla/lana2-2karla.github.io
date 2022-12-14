b3383760bf4f24afaf34a5ea8c064a7f5bd162be
lana2-2karla <allanakarla1702@gmail.com>
2022-12-14 17:52:59 -0300

    :fire: refactor: remove link unicons

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -1,7 +1,6 @@
 function Home () {
   return (
     <div>
-        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
         Homeeeeee
         </div>
   )
