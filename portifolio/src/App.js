b5cea1b2c7a6978b2fb6fe80e28944d689e30ec8
lana2-2karla <allanakarla1702@gmail.com>
2022-12-14 14:33:00 -0300

    :sparkles: feat: react init

---
+++ portifolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from "./pages/home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Home /> }/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ portifolio/src/pages/home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+      <div>Homeeeeee</div>
+    );
+  }
+  
+  export default Home;
