fcc6bbdaaf263152b84604eb3916ca8083d317be
lana2-2karla <allanakarla1702@gmail.com>
2023-05-14 15:36:51 -0300

    :sparkles: feat: import Header

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ResponsiveHeader from '../components/Header'
 
 function Home () {
   return (
     <div>
-      <h1>Homeeeeee</h1>
+      <ResponsiveHeader />
     </div>
   )
 }
