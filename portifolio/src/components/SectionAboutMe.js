bafca7dcf79e0d57ab3ef8a7709e790040985d36
lana2-2karla <allanakarla1702@gmail.com>
2023-05-16 17:35:30 -0300

    :sparkles: feat: component SectionAboutMe

---
+++ portifolio/src/components/SectionAboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+function AboutMe () {
+  return (
+    <Box sx={{ color: 'white' }}>
+      <h1>Sobre mim</h1>
+    </Box>
+  )
+}
+
+export default AboutMe
