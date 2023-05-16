6412f076bc40357de251e3a1edfa0c1210334d4a
lana2-2karla <allanakarla1702@gmail.com>
2023-05-16 18:15:26 -0300

    :construction: animation site

--- portifolio/src/components/SectionAboutMe.js
+++ portifolio/src/components/SectionAboutMe.js
@@ -3,10 +3,12 @@ import Box from '@mui/material/Box'
 
 function AboutMe () {
   return (
-    <Box sx={{ color: 'white' }}>
+    <Box sx={{ color: '#92E3A9' }}>
       <h1>Sobre mim</h1>
     </Box>
   )
 }
 
 export default AboutMe
+
+// https://storyset.com/illustration/webinar/cuate
