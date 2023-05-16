67d62f059b699862e2300490f522e219397c0e25
lana2-2karla <allanakarla1702@gmail.com>
2023-05-16 17:35:53 -0300

    :sparkles: feat: import new section

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -2,14 +2,17 @@ import React from 'react'
 import Box from '@mui/material/Box'
 import ResponsiveHeader from '../components/Header'
 import '../global.css'
+import AboutMe from '../components/SectionAboutMe'
 
 function Home () {
   return (
-    <Box
-      sx={{
-        bgcolor: 'hsla(0, 0%, 0%, 1)'
-      }}>
-        <ResponsiveHeader />
+    <Box sx={{
+      bgcolor: 'hsla(0, 0%, 0%, 1)'
+    }}>
+      <Box>
+          <ResponsiveHeader />
+      </Box>
+      <AboutMe />
     </Box>
   )
 }
