b7a0cf9a5d9259e4e5c24f57e4e0425187ed87e0
lana2-2karla <allanakarla1702@gmail.com>
2023-08-05 16:24:09 -0300

    :sparkles: feat: import AboutMe

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../global.css'
 import Header from '../components/Header'
+import AboutMe from '../components/AboutMe'
 
 function Home () {
   return (
     <div className="home">
       <Header />
+      <AboutMe />
     </div>
   )
 }
