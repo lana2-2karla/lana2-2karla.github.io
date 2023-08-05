a969454041746607722e49b02dd2cde5941a8ae3
lana2-2karla <allanakarla1702@gmail.com>
2023-08-05 17:21:11 -0300

    :sparkles: feat: import Projects

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../global.css'
 import Header from '../components/Header'
 import AboutMe from '../components/AboutMe'
+import Projects from '../components/Projects'
 
 function Home () {
   return (
     <div className="home">
       <Header />
       <AboutMe />
+      <Projects />
     </div>
   )
 }
