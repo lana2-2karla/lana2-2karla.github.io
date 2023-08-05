7c4e51f142c3325a2576bc568a979100721489bb
lana2-2karla <allanakarla1702@gmail.com>
2023-08-05 15:05:29 -0300

    :sparkles: feat: Header

--- portifolio/src/components/Header.js
+++ portifolio/src/components/Header.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Header.css'
+import githubSvg from '../assets/github.svg'
+import linkedinSvg from '../assets/linkedin.svg'
+import logoSvg from '../assets/logo.svg'
 
 function Header () {
   return (
-   <h1>Headerrrr</h1>
+    <div className="header">
+      <div className="left">
+        <div>
+          <img src={logoSvg} alt="logo" className="svg-img-logo" />
+        </div>
+        <p className="portfolio-tab">Portfólio</p>
+      </div>
+      <div className="center">
+        <p className="tab">Projetos</p>
+        <p className="tab">Sobre Mim</p>
+        <p className="tab">Tecnologias</p>
+      </div>
+      <div className="right">
+        <img src={githubSvg} alt="github" className="svg-img-github" />
+        <img src={linkedinSvg} alt="linkedin" className="svg-img-linkedin" />
+      </div>
+    </div>
   )
 }
 
