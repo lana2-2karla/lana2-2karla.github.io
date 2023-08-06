99e9450018a2bfd163930ee4e39b9dc6e145dd7e
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 09:01:04 -0300

    :sparkles: feat: import OtherSkills

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -5,6 +5,7 @@ import Header from '../components/Header'
 import AboutMe from '../components/AboutMe'
 import Projects from '../components/Projects'
 import Technologies from '../components/Technologies'
+import OtherSkills from '../components/OtherSkills'
 
 function Home () {
   return (
@@ -14,6 +15,7 @@ function Home () {
       <Projects />
       <br /> <br /> <br />
       <Technologies />
+      <OtherSkills />
     </div>
   )
 }
