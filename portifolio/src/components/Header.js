3d6853230811668c4d228195b9eb9741d6ca4261
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 12:16:35 -0300

    :ok_hand: style: lint

--- portifolio/src/components/Header.js
+++ portifolio/src/components/Header.js
@@ -7,21 +7,21 @@ import logoSvg from '../assets/logo.svg'
 function Header () {
   return (
     <div className="header">
-      <div className="left">
-        <div>
-          <img src={logoSvg} alt="logo" className="svg-img-logo" />
+        <div className="left">
+            <div>
+            <img src={logoSvg} alt="logo" className="svg-img-logo" />
+            </div>
+            <p className="portfolio-tab">Portfólio</p>
         </div>
-        <p className="portfolio-tab">Portfólio</p>
-      </div>
-      <div className="center">
-        <p className="tab">Projetos</p>
-        <p className="tab">Sobre Mim</p>
-        <p className="tab">Tecnologias</p>
-      </div>
-      <div className="right">
-        <img src={githubSvg} alt="github" className="svg-img-github" />
-        <img src={linkedinSvg} alt="linkedin" className="svg-img-linkedin" />
-      </div>
+        <div className="center">
+            <p className="tab">Projetos</p>
+            <p className="tab">Sobre Mim</p>
+            <p className="tab">Tecnologias</p>
+        </div>
+        <div className="right">
+            <img src={githubSvg} alt="github" className="svg-img-github" />
+            <img src={linkedinSvg} alt="linkedin" className="svg-img-linkedin" />
+        </div>
     </div>
   )
 }
