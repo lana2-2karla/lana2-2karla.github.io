c684b7fffb8eb136216e275dc953f3150dd3b6b0
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 09:48:41 -0300

    :sparkles: feat: import Timeline

--- portifolio/src/pages/home.js
+++ portifolio/src/pages/home.js
@@ -6,6 +6,7 @@ import AboutMe from '../components/AboutMe'
 import Projects from '../components/Projects'
 import Technologies from '../components/Technologies'
 import OtherSkills from '../components/OtherSkills'
+import Timeline from '../components/Timeline'
 
 function Home () {
   return (
@@ -16,6 +17,7 @@ function Home () {
       <br /> <br /> <br />
       <Technologies />
       <OtherSkills />
+      <Timeline />
     </div>
   )
 }
