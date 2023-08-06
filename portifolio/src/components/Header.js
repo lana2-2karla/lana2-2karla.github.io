a6f9535cb91bf24da997654799ae2af4cc5dfe68
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 12:35:45 -0300

    :sparkles: feat: create function handleLinkClick

--- portifolio/src/components/Header.js
+++ portifolio/src/components/Header.js
@@ -5,6 +5,12 @@ import linkedinSvg from '../assets/linkedin.svg'
 import logoSvg from '../assets/logo.svg'
 
 function Header () {
+  const handleLinkClick = (event, sectionId) => {
+    event.preventDefault()
+    const section = document.getElementById(sectionId)
+    section.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <div className="header">
         <div className="left">
@@ -13,10 +19,10 @@ function Header () {
             </div>
             <p className="portfolio-tab">Portfólio</p>
         </div>
-        <div className="center">
-            <p className="tab">Projetos</p>
-            <p className="tab">Sobre Mim</p>
-            <p className="tab">Tecnologias</p>
+        <div>
+            <a href="#projects" onClick={(event) => handleLinkClick(event, 'projects')}>Projetos</a>
+            <a href="#experiences" onClick={(event) => handleLinkClick(event, 'experiences')}>Experiências</a>
+            <a href="#technologies" onClick={(event) => handleLinkClick(event, 'technologies')}>Tecnologias</a>
         </div>
         <div className="right">
             <img src={githubSvg} alt="github" className="svg-img-github" />
