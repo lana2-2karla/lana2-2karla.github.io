3c6d91a925a3b6450a6c0de19a73b8fd4096baeb
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 09:48:09 -0300

    :sparkles: feat: Timeline

---
+++ portifolio/src/components/Timeline.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Timeline.css'
+
+function Timeline () {
+  return (
+    <div className="experience-timeline">
+      <h2>Experiências</h2>
+      <div className="line"></div>
+      <div className="experience">
+        <h3>2021</h3>
+        <p>Started my journey in web development</p>
+      </div>
+      <div className="line"></div>
+      <div className="experience">
+        <h3>2022</h3>
+        <p>Worked as a front-end developer in XYZ Company</p>
+      </div>
+      <div className="line"></div>
+      <div className="experience">
+        <h3>2023</h3>
+        <p>Currently expanding skills in mobile app development</p>
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
