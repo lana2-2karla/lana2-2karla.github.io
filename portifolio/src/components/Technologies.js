05a19803a8eaad863350dc97cbc5c5bfe802f6a3
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 17:58:21 -0300

    :recycle: refactor: technology names and icons

--- portifolio/src/components/Technologies.js
+++ portifolio/src/components/Technologies.js
@@ -1,33 +1,54 @@
 import React from 'react'
 import './Technologies.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faHtml5, faCss3Alt, faJs, faReact, faNode, faPython, faJava, faPhp, faAngular, faVuejs, faSwift, faSass } from '@fortawesome/free-brands-svg-icons'
+import nodeIcon from '../assets/icons/node-js.png'
+import mysqlIcon from '../assets/icons/mysql.png'
+import dockerIcon from '../assets/icons/docker.png'
+import pythonIcon from '../assets/icons/python.png'
+import csharpIcon from '../assets/icons/c-sharp.png'
+import htmlIcon from '../assets/icons/html.png'
+import cssIcon from '../assets/icons/css.png'
+import jsIcon from '../assets/icons/js.png'
+import reactIcon from '../assets/icons/react.png'
+import vueIcon from '../assets/icons/vuejs.svg'
+import jestIcon from '../assets/icons/jest.png'
+import mongodbIcon from '../assets/icons/mongodb.png'
+
+// <a href="https://www.flaticon.com/br/icones-gratis/chapeu-bobo" title="chapéu bobo ícones">Chapéu bobo ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/no-js" title="nó js ícones">Nó js ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/mysql" title="mysql ícones">Mysql ícones criados por Pixel perfect - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/docker" title="docker ícones">Docker ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/pitao" title="pitão ícones">Pitão ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/c-afiado" title="c afiado ícones">C afiado ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/reagir" title="reagir ícones">Reagir ícones criados por amoghdesign - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/html" title="html ícones">Html ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/css" title="css ícones">Css ícones criados por Pixel perfect - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/javascript" title="javascript ícones">Javascript ícones criados por Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/br/icones-gratis/folha" title="folha ícones">Folha ícones criados por Pixel perfect - Flaticon</a>
 
 function Technologies () {
   const technologies = [
-    { name: 'HTML5', icon: 'faHtml5' },
-    { name: 'CSS3', icon: 'faCss3Alt' },
-    { name: 'JavaScript', icon: 'faJs' },
-    { name: 'React', icon: 'faReact' },
-    { name: 'Node.js', icon: 'faNode' },
-    { name: 'Python', icon: 'faPython' },
-    { name: 'Java', icon: 'faJava' },
-    { name: 'PHP', icon: 'faPhp' },
-    { name: 'Angular', icon: 'faAngular' },
-    { name: 'Vue.js', icon: 'aVuejs' },
-    { name: 'Swift', icon: 'faSwift' },
-    { name: 'Sass', icon: 'faSass' }
+    { name: 'Node.js', icon: nodeIcon },
+    { name: 'MongoDB', icon: mongodbIcon },
+    { name: 'MySQL', icon: mysqlIcon },
+    { name: 'Docker', icon: dockerIcon },
+    { name: 'Python', icon: pythonIcon },
+    { name: 'C Sharp', icon: csharpIcon },
+    { name: 'HTML5', icon: htmlIcon },
+    { name: 'CSS', icon: cssIcon },
+    { name: 'JavaScript', icon: jsIcon },
+    { name: 'React', icon: reactIcon },
+    { name: 'Vue.js', icon: vueIcon },
+    { name: 'Jest', icon: jestIcon }
   ]
 
   return (
     <div id="technologies" className="technologies">
       <h2>Tecnologias</h2>
-      <br/><br/> <br/><br/>
       <div className="cardsIcon-container">
         {technologies.map((technology, index) => (
           <div key={index} className="card-icon">
-            <p>{technology.icon}</p>
             <p>{technology.name}</p>
+              <img src={technology.icon} alt={technology.name} className="png-img-icons" />
           </div>
         ))}
       </div>
