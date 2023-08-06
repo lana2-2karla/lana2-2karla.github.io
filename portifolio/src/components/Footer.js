acc8ef194b77d5f64d7355471f8b9f1dabfa62b1
lana2-2karla <allanakarla1702@gmail.com>
2023-08-06 18:56:51 -0300

    :recycle: refactor: add email

--- portifolio/src/components/Footer.js
+++ portifolio/src/components/Footer.js
@@ -4,18 +4,12 @@ import './Footer.css'
 function Footer () {
   return (
     <footer className="footer">
-      <div className="container">
-        <p>© 2023 Your Name. All rights reserved.</p>
-        <div className="social-icons">
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          {/* Add more social icons as needed */}
+        <div className="container">
+            <p>© 2023 Your Name. All rights reserved.</p>
+            <div>
+                <p>Email: allanakarla1702@gmail.com</p>
+            </div>
         </div>
-      </div>
     </footer>
   )
 }
