2a343b6f452bb5ef9504ba2af8fa2904fbd7736d
lana2-2karla <allanakarla1702@gmail.com>
2023-08-08 18:22:51 -0300

    :recycle: refactor: data

--- portifolio/src/components/Projects.js
+++ portifolio/src/components/Projects.js
@@ -6,32 +6,34 @@ function Projects () {
   const projectsData = [
     {
       id: 1,
-      title: 'Projeto 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: 'https://user-images.githubusercontent.com/93014254/207421866-4a739cbe-f82e-4cca-ab7f-7806084e881b.png', // URL da imagem do projeto 1
-      technologies: ['React', 'CSS', 'JavaScript', 'Node.js', 'docker', 'sequelize'],
+      title: 'Save My Dinner',
+      description: 'Simulação de um e-commerce de comidas responsivo, desenvolvido fullstack, permitindo filtrar por nome de restaurante, descrição e prato.',
+      image: 'https://user-images.githubusercontent.com/93014254/207421866-4a739cbe-f82e-4cca-ab7f-7806084e881b.png',
+      technologies: ['React', 'HTML', 'Tailwind', 'javaScript', 'Node.js', 'Mysql', 'Sequelize'],
       githubLink: 'https://github.com/lana2-2karla/food-delivery-app'
     },
     {
       id: 2,
-      title: 'Projeto 2',
-      description: 'Descrição do Projeto 2',
-      image: 'https://via.placeholder.com/200', // URL da imagem do projeto 2
-      technologies: ['HTML', 'CSS', 'JavaScript']
+      title: 'ng-finace',
+      description: 'Simulação de uma API dockerizada para um site interno de transações financeiras. Permite realizar transferências, consultar dados e filtrar transações em cash-in e cash-out.',
+      image: 'https://appmaster.io/api/_files/LLCxhb9Mt9zpfsjf2Rhzhc/download/',
+      technologies: ['Typescript', 'Node.js', 'Express.js', 'PostgreSQL', 'TypeORM', 'JWT', 'Joi', 'docker'],
+      githubLink: 'https://github.com/lana2-2karla/ng-finance'
     },
     {
       id: 3,
-      title: 'Projeto 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: 'https://via.placeholder.com/200', // URL da imagem do projeto 3
-      technologies: ['React', 'Redux', 'Node.js']
+      title: 'CSharp-balta.io-projects',
+      description: 'Projeto introdutório em C# composto por uma coleção de mini projetos do tipo console, incluindo uma calculadora, um editor HTML, um cronômetro e um editor de texto.',
+      image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2018/01/22145352/ONDE-COMECAR-C-SHARP.png',
+      technologies: ['100% C#'],
+      githubLink: 'https://github.com/lana2-2karla/CSharp-balta.io-projects'
     },
     {
       id: 4,
-      title: 'Projeto 4',
-      description: 'Descrição do Projeto 4',
-      image: 'https://via.placeholder.com/200', // URL da imagem do projeto 4
-      technologies: ['Vue.js', 'CSS', 'Firebase']
+      title: 'TPhoneBox',
+      description: 'Projeto fullstack que simula a página inicial de um aplicativo de assinaturas. Oferece todas as informações essenciais para a assinatura mensal de um smartphone.',
+      image: 'https://user-images.githubusercontent.com/93014254/249188454-486890e5-4626-4394-abf9-f7a57e637400.png',
+      technologies: ['Vue.js', 'HTML', 'CSS', 'JavaScrip', 'Node.js', 'Prisma', 'Moongose', 'MySQL', 'MongoDB']
     }
   ]
 
@@ -43,7 +45,7 @@ function Projects () {
           <div key={project.id} className="card">
             <img src={project.image} alt={project.title} className="project-image" />
             <h3>{project.title}</h3>
-            <p>{project.description}</p>
+            <p className='color-grey'>{project.description}</p>
             <p>{project.technologies.join(', ')}</p>
             <br />
             <div className="buttons-container">
